1275684c9dc078a021f3ccc5eb698fcdce7c32d7
Harshit <harshit79090@gmail.com>
2022-05-26 10:18:11 +0530

    Completed with hex part

--- hex.js
+++ hex.js
@@ -1,1 +1,19 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+const color = document.querySelector('.color');
+const btn = document.getElementById('btn');
+
+function getRandomString() {
+  let s = '#';
+  for (let i = 0; i < 6; i++) {
+    s += hex[Math.floor(Math.random() * 100) % hex.length];
+  }
+
+  return s;
+}
+
+btn.addEventListener('click', () => {
+  const randomHex = getRandomString();
+  console.log(randomHex);
+  color.textContent = randomHex;
+  document.body.style.backgroundColor = randomHex;
+});
